651e34d58be12446057b8ee522bdafcc904e9278
신종신 <jongsin16@naver.com>
2024-03-12 22:36:00 +0900

    11403
    graph https://www.acmicpc.net/problem/11403

---
+++ pathfind.cpp
@@ -0,0 +1,33 @@
+//11403 graph
+#include<iostream>
+
+using namespace std;
+
+int arr[101][101];
+
+int main(){
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int k = 0; k < N; k++){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(arr[i][k] && arr[k][j]) arr[i][j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
