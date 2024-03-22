bb030d0831f51385eaa40e9e9de7f11d3fba8aa1
신종신 <jongsin16@naver.com>
2024-03-22 21:46:16 +0900

    31403
    string https://www.acmicpc.net/problem/31403

---
+++ AplusBmiusC.cpp
@@ -0,0 +1,13 @@
+//31403 string
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string a, b, c;
+    cin >> a >> b >> c;
+    cout << stoi(a) + stoi(b) - stoi(c) << "\n";
+    cout << stoi(a + b) - stoi(c);
+    return 0;
+}
