7529337600e15e07f1a28b8157919913a31a1179
신종신 <jongsin16@naver.com>
2024-05-09 22:54:45 +0900

    2292
    math https://www.acmicpc.net/problem/2292

---
+++ hive.cpp
@@ -0,0 +1,17 @@
+//2292 math
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int i = 1;
+    int cnt = 1;
+    while(n > i){
+        i += (6 * cnt);
+        cnt++;
+    }
+    cout << cnt;
+    return 0;
+}
