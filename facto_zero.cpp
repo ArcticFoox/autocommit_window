b6587f8c0836f88160464226a0712c8267c4a67c
신종신 <jongsin16@naver.com>
2024-05-18 22:16:59 +0900

    1676
    math https://www.acmicpc.net/problem/1676

---
+++ facto_zero.cpp
@@ -0,0 +1,18 @@
+//1676 math
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+
+    for(int i = 5; i <= n; i *= 5){
+        cnt += (n/i);
+    }
+
+    cout << cnt;
+    return 0;
+}
