f9862ba1b143c17d1881ada7cd31c74e2f8ecbe5
신종신 <jongsin16@naver.com>
2024-08-13 22:22:25 +0900

    10171
    basic https://www.acmicpc.net/problem/10171

---
+++ cat.cpp
@@ -0,0 +1,12 @@
+//10171 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+    return 0;
+}
