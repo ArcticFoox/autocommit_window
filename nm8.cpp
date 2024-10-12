3c0ca7f27df6807c09f596dd6502bfe13a297ca7
신종신 <jongsin16@naver.com>
2024-10-12 20:33:44 +0900

    15657
    backtracking https://www.acmicpc.net/problem/15657

---
+++ nm8.cpp
@@ -0,0 +1,39 @@
+//15657 backtracking
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int n, m;
+int arr[8];
+int num[8];
+
+void back(int row, int idx){
+    if(row == m){
+        for(int i = 0; i < m; i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i = idx; i < n; i++){
+        if(i < idx) continue;
+        arr[row] = num[i];
+        back(row + 1, i);
+    }
+}
+
+int main(){
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+
+    sort(num, num+n);
+
+    back(0, 0);
+
+    return 0;
+}
