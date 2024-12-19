ddf8e8c9673f6caa3a31bb2c328fe41794a4fd9b
신종신 <jongsin16@naver.com>
2024-12-19 20:53:42 +0900

    23795
    basic https://www.acmicpc.net/problem/23795

---
+++ betting.cpp
@@ -0,0 +1,20 @@
+//23795 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int sum = 0;
+
+    while(true){
+        cin >> n;
+
+        if(n == -1) break;
+
+        sum += n;
+    }
+
+    cout << sum;
+    return 0;
+}
