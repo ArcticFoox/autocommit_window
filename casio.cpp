8681d856df409031ed57bcfed41f9795601ff674
신종신 <jongsin16@naver.com>
2024-12-22 19:50:43 +0900

    15963
    basic https://www.acmicpc.net/problem/15963

---
+++ casio.cpp
@@ -0,0 +1,17 @@
+//15963 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    if(n == m){
+        cout << 1;
+    }   
+    else{
+        cout << 0;
+    }
+    return 0;
+}
