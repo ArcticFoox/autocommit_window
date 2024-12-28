541b2615d9112f4c19b1eddd06876c47195ae532
신종신 <jongsin16@naver.com>
2024-12-28 20:47:32 +0900

    15680
    basic https://www.acmicpc.net/problem/15680

---
+++ yonsei.cpp
@@ -0,0 +1,16 @@
+//15680 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if(n){
+        cout << "Leading the Way to the Future";
+    } else{
+        cout << "YONSEI";
+    }
+    return 0;
+}
