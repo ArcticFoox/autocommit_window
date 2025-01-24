9f1d4246c1243508a7d60081b5764930faaddac6
신종신 <jongsin16@naver.com>
2025-01-24 09:38:46 +0900

    2846
    basic https://www.acmicpc.net/problem/2846

---
+++ uphill.cpp
@@ -0,0 +1,30 @@
+//2846 basic
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    int p[1001];
+    int ans = 0;
+    int l = 1, r = 1;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) cin >> p[i];
+    
+    for(int i = 1; i <= n - 1; i++){
+        if(p[i] < p[i + 1]){
+            r++;
+            ans = max(ans, p[r] - p[l]);
+        }
+        else if(p[i]>=p[i+1]){
+            l = i+1;
+            r = i+1;
+        }
+    }
+
+    cout << ans << '\n';
+    
+    return 0;
+}
